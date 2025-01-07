6c0f02db2f33cbcb325c0ec70f05bbd56005f193
martin0he <martin.hema.mh@gmail.com>
2025-01-06 23:31:34 -0500

    toggle buttons done in explore

--- frontend/src/pages/ExplorePage.tsx
+++ frontend/src/pages/ExplorePage.tsx
@@ -1,9 +1,31 @@
-import { Box, Typography } from "@mui/material";
+import {
+  Box,
+  ToggleButton,
+  ToggleButtonGroup,
+  Typography,
+} from "@mui/material";
 import PageLayout from "./PageLayout";
 import { useState } from "react";
 
 const ExplorePage = () => {
   const [searchInput, setSearchInput] = useState("");
+  const [dataType, setDataType] = useState("recipe");
+  const [chronology, setChronology] = useState("newest");
+
+  const handleDataTypeChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newType: string
+  ) => {
+    setDataType(newType);
+  };
+
+  const handleChronologyChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newChronology: string
+  ) => {
+    setChronology(newChronology);
+  };
+
   return (
     <PageLayout>
       <Box
@@ -20,6 +42,12 @@ const ExplorePage = () => {
           width={{ lg: "28%", md: "28%", sm: "100%", xs: "100%" }}
           minHeight="83vh"
           position="sticky"
+          padding="20px"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="flex-start"
+          rowGap="12px"
           sx={{
             borderRadius: "7px",
             borderStyle: "dashed",
@@ -27,7 +55,131 @@ const ExplorePage = () => {
             backgroundColor: "#F3EDDF",
             borderWidth: "2px",
           }}
-        ></Box>
+        >
+          {/* toggle recipe/cookbook */}
+          <ToggleButtonGroup
+            value={dataType}
+            exclusive
+            fullWidth
+            onChange={handleDataTypeChange}
+            aria-label="Platform"
+            sx={{
+              width: "100%",
+              justifyContent: "center",
+            }}
+          >
+            <ToggleButton
+              value="recipe"
+              sx={{
+                borderRadius: "8px",
+                textTransform: "none",
+                backgroundColor:
+                  dataType === "recipe" ? "primary.dark" : "transparent",
+                border: "1.8px",
+                borderStyle: dataType === "recipe" ? "solid" : "dashed",
+                borderColor: "primary.dark",
+                "&.Mui-selected": {
+                  backgroundColor: "primary.dark",
+                  "&:hover": {
+                    backgroundColor: "primary.dark",
+                  },
+                },
+              }}
+            >
+              <Typography
+                color={dataType === "recipe" ? "white" : "primary.dark"}
+              >
+                recipe
+              </Typography>
+            </ToggleButton>
+            <ToggleButton
+              value="cookbook"
+              sx={{
+                borderRadius: "8px",
+                textTransform: "none",
+                backgroundColor:
+                  dataType === "cookbook" ? "primary.dark" : "transparent",
+                border: "1.8px",
+                borderStyle: dataType === "cookbook" ? "solid" : "dashed",
+                borderColor: "primary.dark",
+                "&.Mui-selected": {
+                  backgroundColor: "primary.dark",
+                  "&:hover": {
+                    backgroundColor: "primary.dark",
+                  },
+                },
+              }}
+            >
+              <Typography
+                color={dataType === "cookbook" ? "white" : "primary.dark"}
+              >
+                cookbook
+              </Typography>
+            </ToggleButton>
+          </ToggleButtonGroup>
+          {/* toggle chronology */}
+          <ToggleButtonGroup
+            value={chronology}
+            exclusive
+            fullWidth
+            onChange={handleChronologyChange}
+            aria-label="Platform"
+            sx={{
+              width: "100%",
+              justifyContent: "center",
+            }}
+          >
+            <ToggleButton
+              value="newest"
+              sx={{
+                borderRadius: "8px",
+                textTransform: "none",
+                backgroundColor:
+                  chronology === "newest" ? "secondary.dark" : "transparent",
+                border: "1.8px",
+                borderStyle: chronology === "newest" ? "solid" : "dashed",
+                borderColor: "secondary.dark",
+                "&.Mui-selected": {
+                  backgroundColor: "secondary.dark",
+                  "&:hover": {
+                    backgroundColor: "secondary.dark",
+                  },
+                },
+              }}
+            >
+              <Typography
+                color={chronology === "newest" ? "white" : "secondary.dark"}
+              >
+                newest
+              </Typography>
+            </ToggleButton>
+            <ToggleButton
+              value="oldest"
+              sx={{
+                borderRadius: "8px",
+                textTransform: "none",
+                backgroundColor:
+                  chronology === "oldest" ? "secondary.dark" : "transparent",
+                border: "1.8px",
+                borderStyle: chronology === "oldest" ? "solid" : "dashed",
+                borderColor: "secondary.dark",
+                "&.Mui-selected": {
+                  backgroundColor: "secondary.dark",
+                  "&:hover": {
+                    backgroundColor: "secondary.dark",
+                  },
+                },
+              }}
+            >
+              <Typography
+                color={chronology === "oldest" ? "white" : "secondary.dark"}
+              >
+                oldest
+              </Typography>
+            </ToggleButton>
+          </ToggleButtonGroup>
+          {/* expected duration slider */}
+        </Box>
         {/* search + body */}
         <Box
           display="flex"
